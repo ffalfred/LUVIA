323268e6817fe25aa48c12f82317d60122ff256e
Alfred Ferrer Florensa <alff@food.dtu.dk>
2025-08-15 18:19:15 +0200

    add files package

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=find_packages(where='src'),
     install_requires=[],
     include_package_data=True,
+    package_data={'luvia': ['data/*.csv']},
     package_dir={'': 'src'},
     entry_points={
         'console_scripts': [
